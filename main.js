73567013cd88a59209f71b68eab21329040a260d
Klyrostack <mirmehran613@gmail.com>
2026-09-17 20:51:26 +0530

    Added search validation and error styling

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const brandLogo = document.querySelector(".brand-logo");
 
 const navLinks = document.querySelectorAll(".nav-link");
 
+const searchForm = document.querySelector(".search-form");
+
+const searchInput = document.querySelector(".search-form input");
+
+const districtSelect = document.querySelector(".search-form select");
+
 brandLogo.addEventListener("click", function (event) {
   event.preventDefault();
   console.log("Brand Logo was clicked");
@@ -26,4 +32,28 @@ navLinks.forEach(function (link) {
 
     console.log("Clicked:", link.textContent);
   });
+});
+// Listener 1: Handles Form Submit.
+
+searchForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let searchQuery = searchInput.value.trim();
+  let district = districtSelect.value;
+
+  if (searchQuery === "") {
+    searchInput.placeholder =
+      "Please enter doctor's name, specialty or clinic!";
+    searchInput.parentElement.classList.add("error");
+    searchInput.focus();
+  } else {
+    console.log(searchQuery);
+    console.log(district);
+  }
+});
+
+// Listener 2: Stands independently outside, listening for typing.
+
+searchInput.addEventListener("input", function () {
+  searchInput.parentElement.classList.remove("error");
 });
